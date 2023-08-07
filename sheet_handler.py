d4da7577669d4ea815b1000c44ab3dbafe09737f
jerryho <jerryjp3914@gmail.com>
2023-08-07 21:41:52 +0800

    feature: list events in parsed text

--- sheet_handler.py
+++ sheet_handler.py
@@ -10,6 +10,31 @@ sheet_url = os.getenv('SHEET_URL')
 gc = pygsheets.authorize(service_file='client_secret.json')
 sheet = gc.open_by_url(sheet_url)
 
+
+def parse_event_data(event_data):
+    # List to store parsed events
+    parsed_events = []
+
+    # Loop through each event in the data
+    for event in event_data:
+        print(event)
+        # Extract the other fields
+        date = event[1]
+        time = event[2]
+        location = event[3]
+        host = event[5]
+        event_id = event[0]
+
+        # Create a formatted string for the event
+        parsed_event = f'Date: {date}, Time: {time}, Location: {location}, Host: {host}, Event ID: {event_id}'
+
+        # Add the parsed event to the list
+        parsed_events.append(parsed_event)
+
+    # Combine the parsed events into a single string
+    parsed_events_str = "\n".join(parsed_events)
+
+    return parsed_events_str
 def add_to_sheet(sheet_name, row):
     # This function will add a row to a Google Sheet
     ws = sheet.worksheet_by_title(sheet_name)
@@ -25,17 +50,17 @@ def fetch_future_events_from_sheet(sheet_name, channel_id, date):
         # Skip the row if it's completely empty
         if all(cell == '' for cell in row):
             continue
+        row_date = '2022-01-01'
         try:
             # Attempt to parse the date
             row_date = parse(row[1], fuzzy=True).date()
             # row_date = parsed_date.strftime('%Y-%m-%d')
         except:
             print(f"Could not parse date: {row[1]}")
-
-        if row[5] == channel_id and row_date >= date:
+        if row[6] == channel_id and row_date >= date:
             future_events.append(row)
 
-    return future_events
+    return parse_event_data(future_events)
 def fetch_participants_by_id(sheet_name, event_id):
     # Select the worksheet by its title
     ws = sheet.worksheet_by_title(sheet_name)
