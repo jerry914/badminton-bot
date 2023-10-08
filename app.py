dde6d2cde57a91e7ed8135892b7df8c27f5458e7
jerryho <jerryjp3914@gmail.com>
2023-10-09 02:51:13 +0800

    fix: add requirements

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def handle_message(event):
     message = TextSendMessage(text=response)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
