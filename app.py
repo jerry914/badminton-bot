b633c9b7c8120929593ce86eb5c73407ac3180be
jerryho <jerryjp3914@gmail.com>
2023-10-09 16:24:46 +0800

    add: chatGPT toggle

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 import sys
+import json
 sys.path.append("gateway")
 import chatGPT_handler
 # take environment variables from .env.
@@ -12,6 +13,8 @@ load_dotenv()
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+
+chatGPT_toggle = False
 app = Flask(__name__)
 
 @app.route("/callback", methods=['POST'])
@@ -27,11 +30,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.message.text)
-    response = chatGPT_handler.run_conversation(event.message.text)
-    print(response)
-    message = TextSendMessage(text=response)
-    line_bot_api.reply_message(event.reply_token, message)
+    global chatGPT_toggle
+    if ("小雀" in event.message.text):
+        chatGPT_toggle = not chatGPT_toggle
+        message = TextSendMessage(text="Assistant is now " + ("ON" if chatGPT_toggle else "OFF"))
+        line_bot_api.reply_message(event.reply_token, message)
+    if (chatGPT_toggle and "小雀" not in event.message.text):
+        response = chatGPT_handler.run_conversation(event.message.text+" userID"+event.source.user_id)
+        message = TextSendMessage(text=response)
+        line_bot_api.reply_message(event.reply_token, message)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5001))
